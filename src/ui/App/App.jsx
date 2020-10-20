aa7de64f2c6d03e63c78b5349a5bb4b967acab35
bugagashinka <voloshyn.andrew@gmail.com>
2020-10-20 16:55:40 +0300

    Initialize. Add base project structure

---
+++ src/ui/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="app">
+      <div className="container">
+        <div className="app__inner"></div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
