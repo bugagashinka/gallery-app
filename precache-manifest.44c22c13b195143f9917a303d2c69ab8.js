c7df37b442c44b57b6a92d9a1dea2a90c4594d8c
bugagashinka <voloshyn.andrew@gmail.com>
2020-11-02 17:14:42 +0200

    Updates

---
+++ precache-manifest.44c22c13b195143f9917a303d2c69ab8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ff6c285d071e9cf2dbf5b73a703675",
+    "url": "/gallery-app/index.html"
+  },
+  {
+    "revision": "2984b8a29e2e4e66ab66",
+    "url": "/gallery-app/static/css/2.336d9dc1.chunk.css"
+  },
+  {
+    "revision": "ec0592d0c03d67c86226",
+    "url": "/gallery-app/static/css/main.f779bfe6.chunk.css"
+  },
+  {
+    "revision": "2984b8a29e2e4e66ab66",
+    "url": "/gallery-app/static/js/2.4395f48a.chunk.js"
+  },
+  {
+    "revision": "5467e851392549ae3564d75adbc67604",
+    "url": "/gallery-app/static/js/2.4395f48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0592d0c03d67c86226",
+    "url": "/gallery-app/static/js/main.4c3992b6.chunk.js"
+  },
+  {
+    "revision": "d988547585aae83fadef",
+    "url": "/gallery-app/static/js/runtime-main.4b039539.js"
+  },
+  {
+    "revision": "50475e9398305457c4ead55941b10a0f",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr-Bold.50475e93.ttf"
+  },
+  {
+    "revision": "ca12a719cd344dd391d9f51e7bbd9b33",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr-Bold.ca12a719.woff"
+  },
+  {
+    "revision": "da9d9adcf6fd8327574a3e5445cbdb52",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr-Bold.da9d9adc.woff2"
+  },
+  {
+    "revision": "510e1c045e61e20233f9ecf23d031703",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr.510e1c04.woff"
+  },
+  {
+    "revision": "ee2c9a200f705b91eb20e1759c6c799b",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr.ee2c9a20.woff2"
+  },
+  {
+    "revision": "fadb2adb0984cb4e053e45428d96e01a",
+    "url": "/gallery-app/static/media/HelveticaNeueCyr.fadb2adb.ttf"
+  },
+  {
+    "revision": "24eef8ae29fa02394dc41a5709e33842",
+    "url": "/gallery-app/static/media/add-icon.24eef8ae.svg"
+  },
+  {
+    "revision": "fe0e2ef9749e289d9064a575b9c8fb8c",
+    "url": "/gallery-app/static/media/cancel-icon.fe0e2ef9.svg"
+  },
+  {
+    "revision": "994e5bf959336eda46183028c0e96e91",
+    "url": "/gallery-app/static/media/close-active-icon.994e5bf9.svg"
+  },
+  {
+    "revision": "efd777f45e12c594d9a9e1d190e2ff17",
+    "url": "/gallery-app/static/media/close-icon.efd777f4.svg"
+  },
+  {
+    "revision": "518dc88ef4829337b75441fae13f5d96",
+    "url": "/gallery-app/static/media/download-icon.518dc88e.svg"
+  },
+  {
+    "revision": "a87b6ca9c5bc7cf01b82ad07222eff3b",
+    "url": "/gallery-app/static/media/edit-icon.a87b6ca9.svg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/gallery-app/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/gallery-app/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/gallery-app/static/media/lg.f4292655.ttf"
+  },
+  {
+    "revision": "9f7e549730fa008c02d7284434770e2a",
+    "url": "/gallery-app/static/media/look-up.9f7e5497.svg"
+  },
+  {
+    "revision": "6c4a5852508600337fe1fd46645cd203",
+    "url": "/gallery-app/static/media/ok-icon.6c4a5852.svg"
+  },
+  {
+    "revision": "0fd1f5477935ec55598041816cf824e2",
+    "url": "/gallery-app/static/media/remove-icon-2.0fd1f547.svg"
+  },
+  {
+    "revision": "682735803c5713a6fca174e201ab8898",
+    "url": "/gallery-app/static/media/remove-icon.68273580.svg"
+  }
+]);
